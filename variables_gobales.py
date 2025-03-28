4ee9a404241d1bb03208a7849effee363de46268
Antonio Fistonich <146194481+AntonioF3011@users.noreply.github.com>
2025-03-27 20:43:28 -0400

    Add files via upload

---
+++ variables_gobales.py
@@ -0,0 +1,44 @@
+months = {
+    1: 'January',
+    2: 'February',
+    3: 'March',
+    4: 'April',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'August',
+    9: 'September',
+    10: 'October',
+    11: 'November',
+    12: 'December'
+}
+
+# palabras_incompletas = [
+#     'de', 'a', 'la', 'el', 'en', 'con', 'por', 'para', 'sin', 'sobre', 
+#     'fue', 'fueron', 'un', 'una', 'unos', 'unas', 'y', 'o', 'ni', 'pero', 
+#     'aunque', 'porque', 'cuando', 'mientras', 'hasta', 'si', 'como', 'que', 
+#     'su', 'sus', 'mi', 'mis', 'tu', 'tus', 'nuestro', 'nuestra', 'nuestros', 
+#     'nuestras', 'vuestro', 'vuestra', 'vuestros', 'vuestras', 'este', 
+#     'esta', 'estos', 'estas', 'ese', 'esa', 'esos', 'esas', 'al', 'del', 
+#     'yo', 'tú', 'él', 'ella', 'nosotros', 'nosotras', 'vosotros', 'vosotras', 
+#     'ellos', 'ellas', 'me', 'te', 'se', 'nos', 'os', 'le', 'les', 'lo', 
+#     'los', 'la', 'las', 'me', 'te', 'se', 'nos', 'os', 'que', 'de', 'en', 
+#     'por', 'para', 'con', 'sin', 'sobre', 'tras', 'entre', 'hacia', 'hasta', 
+#     'desde', 'donde', 'como', 'cual', 'quien', 'cuyo', 'cualquier', 'algún', 
+#     'alguno', 'algunos', 'alguna', 'algunas', 'ningún', 'ninguno', 'ninguna', 
+#     'mucho', 'mucha', 'muchos', 'muchas', 'poco', 'poca', 'pocos', 'pocas', 
+#     'todo', 'toda', 'todos', 'todas', 'varios', 'varias', 'cualquier', 
+#     'otro', 'otra', 'otros', 'otras', 'mismo', 'misma', 'mismos', 'mismas', 
+#     'tal', 'tales', 'cualquiera', 'cualquieras', 'primero', 'primera', 
+#     'primeros', 'primeras', 'último', 'última', 'últimos', 'últimas', 'más', 
+#     'menos', 'mucho', 'mucha', 'muchos', 'muchas', 'tanto', 'tanta', 
+#     'tantos', 'tantas', 'todo', 'toda', 'todos', 'todas', 'algo', 'nada', 
+#     'cada', 'cual', 'alguien', 'nadie', 'cualquiera', 'quienquiera', 
+#     'dondequiera', 'cuál', 'quién', 'cuánto', 'cómo', 'dónde', 'cuándo'
+# ]
+
+contactos = {
+    #'name of contact': "contact number",
+
+}
+
